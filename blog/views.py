bd4afd92e6c62acd107415ca0c95d5ba7abe7382
biagione-svg <tuo@email.com>
2025-03-18 06:22:16 +0700

    Descrizione delle modifiche

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_objects_or_404
+from .models import Post
+def post_list(request):
+    posts=Post.published.all()
+    return render(request, 'blog/post/list.html',
+                  {'posts':posts})
 
 # Create your views here.
